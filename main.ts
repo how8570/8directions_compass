3194fdf9e3aa59c2e2054551340a3a06912ccec4
how8570 <joe13512345@gmail.com>
2020-10-13 23:31:38 +0800

    fix some microbit lib issue

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ let facing_degree = 0
  * 
  * debug 用
  */
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(facing_degree)
+})
 /**
  * 實作角度區間為 ( θ_1 , θ_2 ]   (左開又閉)
  * 
@@ -35,31 +38,58 @@ let facing_degree = 0
  * 
  *                            (202.5)                   (157.5)
  * 
+ * ## 思考面向東方時，北邊在你左手邊(西)
+ * 
+ *      所以EW需互換 如下:
+ * 
+ *                            (337.5)                  (22.5)
+ * 
+ *                                             N(0)
+ * 
+ *                   (315)NE               |                 NW(45)
+ * 
+ *                                 \              |               /
+ * 
+ *       (292.5)                   \          |          /                    (67.5) 
+ * 
+ *                                         \      |     /
+ * 
+ *                  (270)E---------------+----------------W(90)
+ * 
+ *                                         /      |    \
+ * 
+ *       (247.5)                   /          |         \                      (112.5)
+ * 
+ *                                 /              |              \
+ * 
+ *                   (225)SE               |                 SW(135)
+ * 
+ *                                            S(180)
+ * 
+ *                            (202.5)                   (157.5)
+ * 
  * #### 注意北方的邏輯
  * 
  * 可分為
  * 
  * 分為 337.5~0度(360度) "或" 0~22.5度 兩邊
  */
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(facing_degree)
-})
 basic.forever(function () {
     facing_degree = input.compassHeading()
     if (22.5 < facing_degree && facing_degree <= 67.5) {
-        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.NorthWest)
     } else if (67.5 < facing_degree && facing_degree <= 112.5) {
-        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.West)
     } else if (112.5 < facing_degree && facing_degree <= 157.5) {
-        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.SouthWest)
     } else if (157.5 < facing_degree && facing_degree <= 202.5) {
         basic.showArrow(ArrowNames.South)
     } else if (202.5 < facing_degree && facing_degree <= 247.5) {
-        basic.showArrow(ArrowNames.SouthWest)
+        basic.showArrow(ArrowNames.SouthEast)
     } else if (247.5 < facing_degree && facing_degree <= 292.5) {
-        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.East)
     } else if (292.5 < facing_degree && facing_degree <= 337.5) {
-        basic.showArrow(ArrowNames.NorthWest)
+        basic.showArrow(ArrowNames.NorthEast)
     } else if (337.5 <= facing_degree || facing_degree < 22.5) {
         basic.showArrow(ArrowNames.North)
     } else {
