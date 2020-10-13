1502bef11a47c16e64697c1f0ce2f4c468645643
how8570 <joe13512345@gmail.com>
2020-10-13 23:17:21 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://how8570.github.io/8directions_compass/](https://how8570.github.io/8directions_compass/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/how8570/8directions_compass** and import
+
+## Edit this project ![Build status badge](https://github.com/how8570/8directions_compass/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/how8570/8directions_compass** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/how8570/8directions_compass/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,61 @@
+let facing_degree = 0
+/**
+ * 實作角度區間為 ( θ_1 , θ_2 ]   (左開又閉)
+ * 
+ * 參考角度如下:
+ * 
+ *                            (337.5)                  (22.5)
+ * 
+ *                                             N(0)
+ * 
+ *                   (315)NW               |                 NE(45)
+ * 
+ *                                 \              |               /
+ * 
+ *       (292.5)                   \          |          /                    (67.5) 
+ * 
+ *                                         \      |     /
+ * 
+ *                  (270)W---------------+----------------E(90)
+ * 
+ *                                         /      |    \
+ * 
+ *       (247.5)                   /          |         \                      (112.5)
+ * 
+ *                                 /              |              \
+ * 
+ *                   (225)SW               |                 SE(135)
+ * 
+ *                                            S(180)
+ * 
+ *                            (202.5)                   (157.5)
+ * 
+ * #### 注意北方的邏輯
+ * 
+ * 可分為
+ * 
+ * 分為 337.5~0度(360度) "或" 0~22.5度 兩邊
+ */
+basic.forever(function () {
+    facing_degree = input.compassHeading()
+    if (22.5 < facing_degree && facing_degree <= 67.5) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (67.5 < facing_degree && facing_degree <= 112.5) {
+        basic.showArrow(ArrowNames.East)
+    } else if (112.5 < facing_degree && facing_degree <= 157.5) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (157.5 < facing_degree && facing_degree <= 202.5) {
+        basic.showArrow(ArrowNames.South)
+    } else if (202.5 < facing_degree && facing_degree <= 247.5) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (247.5 < facing_degree && facing_degree <= 292.5) {
+        basic.showArrow(ArrowNames.West)
+    } else if (292.5 < facing_degree && facing_degree <= 337.5) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else if (337.5 > facing_degree || facing_degree < 112.5) {
+        basic.showArrow(ArrowNames.North)
+    } else {
+        basic.showString("ERROR")
+        basic.showIcon(IconNames.Sad)
+    }
+})
